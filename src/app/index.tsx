153bcd37f5e08bfda99ddd39f5abeccb211367f7
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-05-30 14:29:17 +0530

    update: main index & layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>index</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default index;
+
+const styles = StyleSheet.create({});
